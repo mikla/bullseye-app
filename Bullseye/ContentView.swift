2b84203841486df911a984d6f3208db1d44f6853
Artsiom Miklushou <mikla.op@gmail.com>
2020-01-08 18:21:25 +0200

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by amiklushou on 08/01/2020.
+//  Copyright © 2020 Artsiom Miklushou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
